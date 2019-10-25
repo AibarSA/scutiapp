e9d0363ccb5c2eb2ad60d1d442e853e7aea427df
Aibar-B <aibro_85@mail.ru>
2019-10-25 14:49:02 +0600

    add fetch rest api from backend

--- src/components/post-list/post-list.component.jsx
+++ src/components/post-list/post-list.component.jsx
@@ -5,10 +5,39 @@ import Post from '../post/post.component';
 import '../../pages/homepage/homepage.styles.scss';
 
 
-const PostList = () => (
-    <div className="w3-col l8 s12">
-        <Post/>
-    </div>
-)
+class PostList extends React.Component {
+
+    constructor(){
+        super();
+        this.state={
+          posts: [],
+          searchField: ''
+        }
+      }
+
+      componentDidMount(){
+        fetch('http://localhost:8181/book/bookList')
+        .then(response => response.json())
+        .then(postlist => this.setState({posts: postlist}));
+        
+      };
+    
+    
+      render(){
+          return(
+            <div className="w3-col l8 s12">
+               {
+                   this.state.posts.map(({title, description, id, publicationDate}) => (
+                      <Post key={id} title={title} description={description} publicationDate={publicationDate}/>
+                   ))
+               }
+            </div>
+          )
+      }
+
+        
+
+}
+
 
 export default PostList;
